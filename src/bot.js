62a4cfcc42f1b733d5aab64d64bd1d3c1302e806
alfathdirk <alfathdirk@gmail.com>
2017-09-02 23:06:24 +0700

    v2.0

--- src/bot.js
+++ src/bot.js
@@ -1,12 +1,7 @@
 const LineConnect = require('./connect');
 let LINE = require('./main.js');
 
-const auth = {
-	authToken: 'EkhKpJrY4uAEW0eLJtQb.DJL32RkCVw/GY+QFfzT2IW.Glp/DCyaiJSF7+yDbCsP9OHsXzdJvE/cHOhGu6pqDAA=',
-	certificate: '2c0a5dbde000c76c2fa05510742ac44ac6f44a26cb78dd83304caf5da0273371'
-}
-
-let client =  new LineConnect(auth);
+let client =  new LineConnect();
 
 client.startx().then(async (res) => {
 	let ops;
