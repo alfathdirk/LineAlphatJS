1229f2434a80b67f9bffd2713fd488a73151bcae
alfathdirk <alfathdirk@gmail.com>
2017-09-03 17:36:20 +0700

    add check reader by tag and fix some error

--- src/bot.js
+++ src/bot.js
@@ -1,12 +1,7 @@
 const LineConnect = require('./connect');
 let LINE = require('./main.js');
 
-const auth = {
-	authToken: 'EkYmu9gJ8OQyw46Iua2b.DJL32RkCVw/GY+QFfzT2IW.u5XTfi3+RFzTfvEwcpKbq8QB1XFN/9tpQXs2ddXt5BQ=',
-	certificate: 'c33dfd02378ef60b4a63df7d40c2c6b7704eec6b30d764f8cc25712093dc2677',
-}
-
-let client =  new LineConnect(auth);
+let client =  new LineConnect();
 
 client.startx().then(async (res) => {
 	let ops;
