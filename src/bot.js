1c22902c94580fdba2c093e7495ecaa4ec529261
alfathdirk <alfathdirk@gmail.com>
2017-09-08 10:50:45 +0700

    add bot.js

---
+++ src/bot.js
@@ -0,0 +1,27 @@
+const LineConnect = require('./connect');
+let LINE = require('./main.js');
+
+// const auth = {
+// 	authToken: 'token here',
+// 	certificate: 'cert here',
+// }
+
+// let client =  new LineConnect(auth);
+let client =  new LineConnect();
+
+client.startx().then(async (res) => {
+	let ops;
+	while(true) {
+		try {
+			ops = await client.fetchOps(res.operation.revision, 5);
+		} catch(error) {
+			console.log('error',error)
+		}
+		for (let op in ops) {
+			if(ops[op].revision.toString() != -1){
+				res.operation.revision = ops[op].revision;
+				LINE.poll(ops[op])
+			}
+		}
+	}
+});
